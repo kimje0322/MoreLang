b43b6c339c340adcebd3095b0f15e1d6f00dfe98
philsang <gofeel8@hanmail.net>
2020-10-19 19:49:29 +0900

    [FE]Add Video 비디오페이지추가

--- frontend/morelanguage/src/main.js
+++ frontend/morelanguage/src/main.js
@@ -2,11 +2,14 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import VueYoutube from "vue-youtube";
 
 Vue.config.productionTip = false;
+
+Vue.use(VueYoutube);
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
---
+++ frontend/morelanguage/src/views/Video.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="video">
+   <h3>비디오페이지</h3>
+   <youtube :video-id="videoId" ref="youtube" @playing="playing"></youtube>
+<button @click="playVideo">play</button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Video",
+    data() {
+    return {
+      videoId: 'lG0Ys-2d4MA'
+    }
+  },
+  methods: {
+    playVideo() {
+      this.player.playVideo()
+    },
+    playing() {
+      console.log(' we are watching!!!')
+    }
+  },
+  computed: {
+    player() {
+      return this.$refs.youtube.player
+    }
+  }
+};
+</script>
