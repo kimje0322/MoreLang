18c32b0bbb99c572197635d5e032f1d9084e8ed2
정성오 <wnw1102@naver.com>
2020-10-21 13:03:26 +0900

    [FE/BE] Add googleLogin

--- frontend/morelanguage/src/views/Home.vue
+++ frontend/morelanguage/src/views/Home.vue
@@ -16,12 +16,33 @@
           </div>
 
           <!-- 구글 로그인 -->
-          <div></div>
+          <div>
+            <v-btn text @click="login" class="" v-if="!member.name">Login</v-btn>
+            <v-menu open-on-hover offset-y v-else-if="member.name" no-gutters>
+              <template v-slot:activator="{ on, attrs }">
+                <v-card color="transparent" v-bind="attrs" v-on="on" flat>
+                  <v-row no-gutters>
+                    <v-col cols="4" class="d-nome d-md-flex">
+                      <v-avatar>
+                        <v-img max-height="100%" :src="member.profileImg" alt="유저썸네일"></v-img>
+                      </v-avatar>
+                    </v-col>
+                    <v-col cols="8">
+                      <div class="text-left subtitle">{{ member.name }}</div>
+                    </v-col>
+                  </v-row>
+                </v-card>
+                </template>
+            </v-menu>
+          </div>
 
           <!-- 메뉴바 -->
           <!-- <div style="width: 50px; ">
             <v-icon>hamburger-menu</v-icon>
           </div> -->
+        </div>
+        <div>
+          <button @click="logout">logout</button>
         </div>
       </header>
       <!-- 소개 영상 -->
@@ -48,10 +69,57 @@
   </div>
 </template>
 
+<script src="https://apis.google.com/js/platform.js"></script>
 <script>
-// @ is an alias to /src
-
+import axios from "axios"
 export default {
-  name: "Home"
+  name: "Home",
+  data(){
+    return {
+      gauth: {},
+      member:{},
+      refreshToken:"",
+      user:{}
+    }
+  },
+  mounted(){
+    gapi.load('auth2', ()=> { 
+        this.gauth = gapi.auth2.init({
+          client_id: '258439612277-a2k3f6ro1jvdkbois85pt4cngrs6hctk.apps.googleusercontent.com'
+        });      
+        this.gauth.then(function(){
+            console.log('init success');
+        }, function(){
+            console.error('init fail');
+        })
+    });
+  },
+  methods : {
+    login() {
+    this.gauth.grantOfflineAccess()
+    .then((data)=>{
+      console.log(data.code);
+      const fd = new FormData();
+      fd.append("code", data.code);
+      fd.append("redirect", window.location.href)
+      axios.post('http://localhost:8080/login',fd)
+      .then((response)=>{
+        console.log("성공!")
+        this.member = response.data.member
+        console.log(this.member)
+        this.refreshToken = response.data.refreshToken;
+      })
+    });
+    },
+    logout(){
+      this.user = this.gauth.currentUser.get()
+      this.user.disconnect()
+      .then(()=>{
+        this.member ={}
+        this.refreshToken = ""
+        this.user={}
+      });
+    },
+  }
 };
 </script>
