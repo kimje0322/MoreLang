3753d4e34f29a31f60d5e749bbcfc5679cfdd4a8
김지은 <happyday751@naver.com>
2020-10-27 20:09:40 +0900

    [FE] Add search dictionary #S03P31B207-78

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,40 +1,168 @@
 <template>
 <!-- 상단 -->
   <div class="Navbar">
-    <div >
+    <div>
       <v-card-title>
         <router-link to="/">
           <h2 class="logo px-5 py-1" style="display:inline-block">Morelang</h2>
         </router-link>
-        <div class="mx-auto" style="display:inline-block; width: 33%;"> 
-          <v-text-field
-            height="37"
-            hide-details=""
-            placeholder='키워드 검색'
-            outlined clearable
-            prepend-inner-icon="mdi-magnify"
-            
-          />
+        <div class="mx-auto mt-1">
+          <!-- 검색창 -->
+          <form style="display:inline-block; width:385px;" action="/search" method="get">
+            <v-text-field
+              v-model="keyword"
+              @keyup.enter="onSearch"
+              height="37"
+              hide-details=""
+              placeholder='키워드 검색'
+              outlined clearable
+              prepend-inner-icon="mdi-magnify"
+            />
+          </form>
+          <!-- 번역버튼 -->
+          <div class="mx-1"  style="display:inline-block">
+            <v-btn
+              @click="beforeTrans"
+              class="py-4 mb-2"
+              x-small
+              color="#43A047"
+              dark
+            >
+              <v-icon>mdi-google-translate</v-icon>
+            </v-btn>
           </div>
-        <span class="mr-5">로그아웃</span>
+        </div>
+          <router-link to="/mypage">
+            <p class="navBtn my-auto mr-3">마이페이지</p>
+            <!-- <v-avatar class="mr-3" color="indigo" size="38">
+              <v-icon dark>
+                mdi-account-circle
+              </v-icon>
+            </v-avatar> -->
+          </router-link>
+          <p class="navBtn mr-2 my-auto">로그아웃</p>
+          <!-- <v-icon size="25" class="mr-3">mdi-logout-variant</v-icon> -->
       </v-card-title>
     </div>
-</div>
+
+    <!-- 검색어 없이 번역버튼 눌렀을 경우 스낵바 -->
+    <v-container
+      fluid
+      class="text-center"
+      style="height:1px; padding-bottom:0px"
+    >
+      <!-- <v-row
+        class="flex"
+        justify="space-between"
+      > -->
+        <v-col
+          cols="12"
+          class="mt-2"
+          style="height:0px!important;"
+        >
+          <v-tooltip
+            v-model="errSnackbar"
+            top
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                icon
+                v-bind="attrs"
+                v-on="on"
+              >
+              </v-btn>
+            </template>
+            <v-icon color="white" class="mr-1">mdi-alert-circle-outline</v-icon>
+            <span>검색어를 입력해주세요</span>
+          </v-tooltip>
+        </v-col>
+      <!-- </v-row> -->
+    </v-container>
+
+    <!-- 번역 언어 모달 -->
+    <v-dialog v-model="transDialog" scrollable max-width="300px">
+    <v-card>
+      <v-toolbar
+        color="#43A047"
+        dark
+      >
+        <v-toolbar-title class="toolbarTitle">
+          <p class="my-auto" style="margin-left: 85px; text-align:center!important">검색어 번역</p>
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn icon>
+        </v-btn>
+      </v-toolbar>
+      <v-card-text style="height: 300px;"> 
+        <v-card class="mx-auto mt-5" max-width="500" flat>
+          <v-row justify="center">
+            <v-container class="language" style="text-align:center;">
+              <v-row no-gutters>
+                <!-- 언어 선택 -->
+                <v-col v-for="(kr, en) in language" :key="en" cols="12" sm="12">
+                  <v-card
+                    @click="onTranslate(en)"
+                    class="pa-2 selectLang"
+                    outlined
+                    tile
+                  >
+                    <p class="my-auto">{{kr}}</p>
+                  </v-card>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-row>
+        </v-card>
+        </v-card-text>
+        <v-divider class="mb-0"></v-divider>
+        <v-card-actions>
+          <v-btn color="black" text block class="mx-auto" @click="transDialog = false">닫기</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
 
 </template>
 
 <script scoped>
 import "@/../public/css/Navbar.scss"
-  export default {
-    data () {
-      return {
-        items: [
-          { title: 'Home', icon: 'mdi-home-city' },
-          { title: 'My Account', icon: 'mdi-account' },
-          { title: 'Users', icon: 'mdi-account-group-outline' },
-        ],
+import axios from "axios";
+const SERVER_URL = "https://morelang.gq/api"
+
+  export default {  
+    updated() {
+      if (this.errSnackbar && this.keyword) {
+        this.errSnackbar = false;
       }
     },
+    data () {
+      return {
+        keyword:'',
+        transDialog: false,
+        errSnackbar: false,
+        timeout: 1500,
+        errtext:'검색어를 입력해주세요',
+        language: {kr:'한국어', en:'영어', jp: '일본어', cn: '중국어', vi: '베트남어', id:'인도네시아어', ar: '아랍어', bn:'뱅갈어', de:'독일어', es: '스페인어', fr:'프랑스어', hi:'힌디어', it: '이탈리아어', ms:'말레이시아어', nl:'네덜란드어', pt:'포르투갈어', ru:'러시아', th:'태국어', tr: '터키어'}
+      }
+    },
+    methods: {
+      beforeTrans() {
+        if (this.keyword) {
+          this.transDialog = true
+        } else {
+          this.errSnackbar = true;
+        }
+        
+      },
+      onTranslate(lang) {
+        
+        axios
+          .get(`${SERVER_URL}/translate?query=${this.keyword}&src_lang=kr&target_lang=${lang}`)
+          .then((res) => {
+            this.keyword = res.data
+          })
+      },
+    }
   }
 </script>
 
@@ -44,5 +172,26 @@ import "@/../public/css/Navbar.scss"
   }
   a {
     color: black !important;
+    text-decoration: none !important;
   }
+  .navBtn {
+    font-family: 'Nanum Gothic', sans-serif;
+    /* font-family: 'Do Hyeon', sans-serif; */
+    font-size: 17.5px;
+    color: #616161
+  }
+  .selectLang:hover {
+    background-color: #def5df;
+  }
+  .language {
+    font-family: 'Nanum Gothic', sans-serif;
+  }
+  .v-snack__wrapper {
+    min-width: none;
+    min-height: none;
+    }
+  .container text-center container--fluid {
+    min-height: none;
+  }
+
 </style>
--- frontend/src/views/Mypage/VideoLearning.vue
+++ frontend/src/views/Mypage/VideoLearning.vue
@@ -1,12 +1,43 @@
 <template>
   <div>
     <h3 class="title">닉네임님의 영상</h3>
+    <div v-for="(video, i) in videoLst" :key="i" style="display: inline-block;">
+      <div
+          class="col"
+          style=" margin: auto; display: inline-block;"
+        >
+          <div style="margin-bottom: 10px;">
+            <img
+              style="width:300px"
+              :src="video.imgUrl"
+              alt=""
+            />
+          </div>
+          <h3 v-if="video.title.length > 38">{{ video.title.substring(0, 36) }} ...</h3>
+          <h3 v-else>{{video.title}}</h3>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+const SERVER_URL = "https://morelang.gq/api"
+
 export default {
-  
+  data() {
+    return {
+      videoLst: []
+    }
+  },
+  mounted() {
+    axios
+      .get(`${SERVER_URL}/search?q=glee&start=0`)
+      .then((res) => {
+        console.log(res)
+        this.videoLst = res.data;
+      })
+  },
 }
 </script>
 
