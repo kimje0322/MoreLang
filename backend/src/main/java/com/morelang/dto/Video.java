da620e0041a242fd8912e2e4a7815a3da252c779
박진용 <jypark5826@gmail.com>
2020-10-28 11:20:01 +0900

    [BE] add video controller/service

---
+++ backend/src/main/java/com/morelang/dto/Video.java
@@ -0,0 +1,22 @@
+
+package com.morelang.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Video {
+
+	private String id;
+	private String title;
+	private String description;
+	private String publishedAt;
+	private String channelId;
+	private String channelTitle;
+	private String defaultLanguage;
+	private String defaultAudioLanguage;
+
+}
