af62de945a852442041a00faa93a9abb36a23eff
philsang <gofeel8@hanmail.net>
2020-10-28 23:38:23 +0900

    [FE] add video mode2 완성

--- frontend/src/views/Video.vue
+++ frontend/src/views/Video.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="video">
     <Navbar />
+    
+    <button @click="changeMode(1)">Mode1</button>  
+    <button @click="changeMode(2)">Mode2</button>  
+    <button @click="changeMode(3)">Mode3</button>  
+  <input type="button" @click="increment()" value="increment"/>
     <h3 id="page">비디오페이지</h3>
     <youtube id="ytp" :video-id="videoId" ref="youtube" :nocookie="true"  :player-vars="playerVars" @ready="getCaptionsList"  @paused="sayHi"  @playing="playing"></youtube>
     <button @click="playVideo">play</button>  
@@ -13,6 +18,7 @@
     </div>
     <div>timer: {{timer}}</div>
     <div>state: {{state}}</div>
+    <div>mode: {{mode}}</div>
     <button @click="beforeCaption">이전문장</button>  
     <button @click="nextCaption">다음문장</button>  
     <div><h2>  대사: {{nowText}}</h2></div>
@@ -50,9 +56,10 @@ export default {
   },
   data() {
     return {
+      mode : 1,
       unit : 10,
       nowText : "",
-      nowIdx : -1,
+      nowIdx : 0,
       elements : null,
       videoId: "DFjIi2hxxf0",
       selectedLang : "",
@@ -72,6 +79,14 @@ export default {
     };
   },
   methods: {
+        increment: function () {
+      this.$store.commit('increment')
+    },
+    changeMode(num){
+      this.mode = num;
+      console.log("mode = ",this.mode);
+
+    },
     nextCaption(){
         console.log("다음문장idx",this.nowIdx+1);
         // console.log(this.caption[this.nowIdx+1]._text);
@@ -172,21 +187,48 @@ export default {
         // this.timer =this.player.getCurrentTime();
         await this.player.getCurrentTime().then(data => this.timer=data);
           
-          var tf = false;
+          // var tf = false;
+          var tempIdx= -1;
           if(this.elements != null){
-            
-            this.elements.forEach((el,i) =>{
+            // console.log(Array.from(this.elements));
+            Array.from(this.elements).some((el,i) =>{
               if (el.dataset.start <= this.timer &&this.timer < el.dataset.end){
                 el.classList.add("current");
+                // console.log(this.elements[this.nowIdx].className);
+                // this.elements[this.nowIdx].classList.remove("current");
+                
+
                 this.nowText = el.innerHTML;
-                this.nowIdx = i;
-                tf = true;
+                // this.nowIdx = i;
+                tempIdx = i;
+                // tf = true;
+                return true;
               } 
-              else el.classList.remove("current");
+              // else el.classList.remove("current");
+             
             });
+            if(tempIdx != -1){
+              // console.log("-1아니네?")
+              if(tempIdx != this.nowIdx){
+                if(this.mode == 2){
+                  this.seekVideo(this.caption[this.nowIdx]._attributes.start);
+                  
+                }else{
+                  
+                  this.elements[this.nowIdx].classList.remove("current");
+                  this.nowIdx = tempIdx;
+                  this.elements[this.nowIdx].classList.add("current");
+                }
+              }
+            }else{
+              console.log("아무말도안하나");
+              this.nowText="";
+              this.elements[this.nowIdx].classList.remove("current");
+              // this.seekVideo(this.caption[this.nowIdx]._attributes.start);
+            }
           
           }
-          if(tf == false)this.nowText="";
+          // if(tf == false)this.nowText="";
           // console.log(this.caption[this.nowIdx]._text);
           if(this.elements != null){
             // console.log(this.elements);
@@ -236,7 +278,7 @@ export default {
       this.getCaption();
     },
     nowIdx : function(){
-      
+      console.log("문장바뀜");
     }
 
   },
