cc234c7749978e5e889ffc92539ff96cffbf739d
김지은 <happyday751@naver.com>
2020-10-29 15:40:39 +0900

    [FE]Update Mypage and Quiz

--- frontend/src/views/Mypage/VideoLearning.vue
+++ frontend/src/views/Mypage/VideoLearning.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h3 class="title">닉네임님의 영상</h3>
-    <!-- <div v-for="(video, i) in videoLst" :key="i" style="display: inline-block;">
+    <div v-for="(video, i) in videoLst" :key="i" style="display: inline-block;">
       <div
           class="col"
           style=" margin: auto; display: inline-block;"
@@ -16,13 +16,13 @@
           <h3 v-if="video.title.length > 38">{{ video.title.substring(0, 36) }} ...</h3>
           <h3 v-else>{{video.title}}</h3>
       </div>
-    </div> -->
+    </div>
   </div>
 </template>
 
 <script>
-// import axios from "axios";
-// const SERVER_URL = "https://morelang.gq/api"
+import axios from "axios";
+const SERVER_URL = "https://morelang.gq/api"
 
 export default {
   data() {
@@ -30,14 +30,14 @@ export default {
       videoLst: []
     }
   },
-  // mounted() {
-  //   axios
-  //     .get(`${SERVER_URL}/search?q=glee&start=0`)
-  //     .then((res) => {
-  //       console.log(res)
-  //       this.videoLst = res.data;
-  //     })
-  // },
+  mounted() {
+    axios
+      .get(`${SERVER_URL}/search?q=glee&start=0`)
+      .then((res) => {
+        console.log(res)
+        this.videoLst = res.data;
+      })
+  },
 }
 </script>
 
