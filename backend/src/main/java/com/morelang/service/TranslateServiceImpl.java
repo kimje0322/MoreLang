a2655e742468b89ea5be2ab78e2ce7c96648e077
박진용 <jyp5826@naver.com>
2020-11-03 08:20:18 +0000

    Revert "Merge branch 'feat/back/pay' into 'develop'"
    This reverts merge request !87

--- backend/src/main/java/com/morelang/service/TranslateServiceImpl.java
+++ backend/src/main/java/com/morelang/service/TranslateServiceImpl.java
@@ -16,32 +16,20 @@ public class TranslateServiceImpl implements TranslateService {
 	final static String REST_API_KEY = "e852b5b6b1d40103d1bc8dfa69a0bd6c";
 
 	@Override
-	public String translate(String query) throws Exception {
-		String s = null;
-
+	public String translate(String src_lang, String target_lang, String query) throws Exception {
 		if (query.equals("")) {
-			return s;
+			return "";
 		}
 
-		URL url = new URL(
-				"https://dapi.kakao.com/v3/translation/language/detect?query=" + URLEncoder.encode(query, "utf-8"));
+		URL url = new URL("https://dapi.kakao.com/v2/translation/translate?" + "src_lang=" + src_lang + "&target_lang="
+				+ target_lang + "&query=" + URLEncoder.encode(query, "utf-8"));
 		Map<String, String> props = new HashMap<>();
+		props.put("Content-Type", "application/x-www-form-urlencoded;charset=utf-8");
 		props.put("Authorization", "KakaoAK " + REST_API_KEY);
-		JSONObject obj = new JSONObject(Https.get(url, props)).getJSONArray("language_info").getJSONObject(0);
-		String code = obj.getString("code");
-		double confidence = obj.getDouble("confidence");
+		JSONObject obj = new JSONObject(Https.get(url, props));
 
-		if (confidence >= 0.99) {
-			url = new URL("https://dapi.kakao.com/v2/translation/translate?" + "src_lang=" + code
-					+ "&target_lang=kr&query=" + URLEncoder.encode(query, "utf-8"));
-			props = new HashMap<>();
-			props.put("Content-Type", "application/x-www-form-urlencoded;charset=utf-8");
-			props.put("Authorization", "KakaoAK " + REST_API_KEY);
-			obj = new JSONObject(Https.get(url, props));
-
-			s = obj.toString().replace("\"", "");
-			s = s.substring(19, s.length() - 3);
-		}
+		String s = obj.toString().replace("\"", "");
+		s = s.substring(19, s.length() - 3);
 
 		return s;
 	}
