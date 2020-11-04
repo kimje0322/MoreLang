8f4da63db10f2c7f6b2f436a81e98affdf111420
박현영 <dig03089@gmail.com>
2020-11-04 14:18:49 +0900

    [FE] fix pay

--- frontend/src/views/Pay/Pay.vue
+++ frontend/src/views/Pay/Pay.vue
@@ -17,11 +17,13 @@ export default {
   data() {
     return {
       // 이 부분 고정값으로 되어있는거 변경 필요
-      item_name: '100보석',
-      total_amount: '10000',
+      item_name: '',
+      total_amount: 0,
     };
   },
   mounted() {
+    this.item_name = this.$route.params.target/100 + "보석"
+    this.total_amount = this.$route.params.target;
     axios.get(`${SERVER_URL}/pay/ready?item_name=${this.item_name}&total_amount=${this.total_amount}`).then(res => {
       this.$store.commit('setPayReady', res.data);
       window.open(res.data.next_redirect_pc_url, "_blank", 'width=450, height=500');
