aab22baae68d725c999aa02658fcdbdc71f76511
정성오 <wnw1102@naver.com>
2020-11-05 13:23:23 +0900

    [FE/BE] 2차 수정

---
+++ frontend/src/views/Account/oauth2redirect.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+    name: "oauth2redirect",
+    created() {
+        console.log("create oauth2redirect");
+        console.log(this.$route.query)
+        const code = this.$route.query.code;
+        if (code) {
+            const fd = new FormData();
+            fd.append("code", code);
+            axios
+                .post(`${this.$store.state.ServerURL}/newuser/naverlogin`, fd)
+                .then(response => {
+                    console.log("성공!");
+                    console.log(response);
+            });
+            this.$router.replace('/Login')
+        }
+        else {
+            this.$router.replace("/")
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
