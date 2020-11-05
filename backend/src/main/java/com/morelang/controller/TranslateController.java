17464a8bb3a5bd4eb378dd60195068802fc857c3
공필상 <gofeel8@hanmail.net>
2020-11-05 06:04:43 +0000

    Revert "Merge branch 'develop' of https://lab.ssafy.com/s03-final/s03p31b207 into develop"
    This reverts commit 5c15008ae6e25cc2bea902fd291db437072953f0

--- backend/src/main/java/com/morelang/controller/TranslateController.java
+++ backend/src/main/java/com/morelang/controller/TranslateController.java
@@ -20,7 +20,7 @@ public class TranslateController {
 	@Autowired
 	TranslateService translateService;
 
-	@GetMapping("/newuser/translate")
+	@GetMapping("/translate")
 	@ApiOperation(value = "한국어kr / 영어en / 일본어jp / 중국어cn / 베트남어vi / 인도네시아어id / 아랍어ar / 뱅갈어bn / 독일어de / 스페인어es / 프랑스어fr / 힌디어hi / 이탈리아어it / 말레이시아어ms / 네덜란드어nl / 포르투갈어pt / 러시아어ru / 태국어th / 터키어tr")
 	public ResponseEntity<String> translate(@RequestParam("src_lang") String src_lang,
 			@RequestParam("target_lang") String target_lang, @RequestParam("query") String query,
