3f27ee027e45864cc6d24a3c1277249b0914d6bb
정성오 <wnw1102@naver.com>
2020-11-08 15:29:34 +0900

    [BE] fix security config

--- backend/src/main/java/com/morelang/config/SecurityConfig.java
+++ backend/src/main/java/com/morelang/config/SecurityConfig.java
@@ -39,7 +39,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	// Swagger 관련 문서는 모두가 볼 수 있도록 설정하기 위해 WebSecurity ignore 설정 진행
 	@Override 	
 	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().antMatchers("/v2/api-docs", "/swagger-resources/**", "/swagger-ui.html", "/webjars/**", "/swagger/**", "/webSocket/**");
+		web.ignoring().antMatchers("/v2/api-docs", "/swagger-resources/**", "/swagger-ui.html", "/webjars/**", "/swagger/**", "/newuser/**");
 	}
 
 	@Override
