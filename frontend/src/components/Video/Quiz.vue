f3f47921d2fd4dcd434b8bd45cba4b75d9fbde1d
정성오 <wnw1102@naver.com>
2020-11-12 17:14:53 +0900

    [FE] fix query

--- frontend/src/components/Video/Quiz.vue
+++ frontend/src/components/Video/Quiz.vue
@@ -91,10 +91,7 @@ export default {
           for (var i=0; i<quizInput.length; i++) {
             if (quizInput[i] === ' _ ') { 
                 this.quizBox.push({index: j++, quiz: 'blank'});
-            } else if (quizInput[i].startsWith(' _ ')) {
-                this.quizBox.push({index: j++, quiz: 'blank'})
-                this.quizBox.push({index: 0, quiz: quizInput[i].slice(6)})
-            } else {
+            }else {
               this.quizBox.push({index: 0, quiz: quizInput[i]})    
             }
           }
