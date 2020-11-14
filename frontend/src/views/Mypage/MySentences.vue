3c566be6e70612bc2959085542f47d5bc5d0798a
박현영 <dig03089@gmail.com>
2020-11-14 16:47:57 +0900

    pull develop

--- frontend/src/views/Mypage/MySentences.vue
+++ frontend/src/views/Mypage/MySentences.vue
@@ -1,8 +1,8 @@
 <template>
-  <v-container style="margin-top: 15px;">
-    <h1 >스크랩한 문장</h1>
-    <br>
-    <v-expansion-panels style=" width:70%;">
+  <v-container style="margin-top: 15px">
+    <h1>스크랩한 문장</h1>
+    <br />
+    <v-expansion-panels style="width: 70%">
       <v-expansion-panel
         v-for="(sentence, i) in setences"
         :key="i"
@@ -21,32 +21,9 @@
             @click="delSentence(sentence.scrapId)"
             >mdi-close</v-icon
           >
-          <!-- </v-btn> -->
         </v-expansion-panel-content>
       </v-expansion-panel>
     </v-expansion-panels>
-    <!-- <v-row no-gutters style="text-align:center; width:70%;"> -->
-    <!-- <v-col v-for="(sentence, i) in setences" :key="i"> -->
-    <!-- <v-card class="pa-2 selectLang" outlined tile style="position:relative"> -->
-    <!-- append-icon="mdi-lead-pencil" label="복습" -->
-    <!-- <v-checkbox
-            style="width: 27%;
-                        position: absolute;
-                        bottom: 7px;
-                left: 5px;"
-            color="success"
-            value="success"
-            hide-details
-          ></v-checkbox> -->
-
-    <!-- <v-icon small left>mdi-lead-pencil</v-icon>
-              복습 -->
-
-    <!-- <p class="my-auto">{{ sentence.sentence }}</p> -->
-    <!-- <p class="my-auto">{{ sentence.memo }}</p> -->
-    <!-- </v-card> -->
-    <!-- </v-col> -->
-    <!-- </v-row> -->
   </v-container>
 </template>
 
@@ -56,11 +33,11 @@ import axios from "@/plugins/axios";
 export default {
   data() {
     return {
-      setences: []
+      setences: [],
     };
   },
   mounted() {
-    axios.get(`/user/myscrap?direction=ASC&page=0&size=100000`).then(res => {
+    axios.get(`/user/myscrap?direction=ASC&page=0&size=100000`).then((res) => {
       console.log("여기요!");
       console.log(res.data.content);
       this.setences = res.data.content;
@@ -68,17 +45,19 @@ export default {
   },
   methods: {
     delSentence(sid) {
-      axios.delete(`/user/delete-scrap?scrapId=${sid}`).then(res => {
-        axios.get(`/user/myscrap?direction=ASC&page=0&size=100000`).then(res => {
-          console.log("여기요!");
-          console.log(res.data.content);
-          this.setences = res.data.content;
-        });
+      axios.delete(`/user/delete-scrap?scrapId=${sid}`).then((res) => {
+        axios
+          .get(`/user/myscrap?direction=ASC&page=0&size=100000`)
+          .then((res) => {
+            console.log("여기요!");
+            console.log(res.data.content);
+            this.setences = res.data.content;
+          });
         console.log(res);
         console.log("삭제완료!");
       });
-    }
-  }
+    },
+  },
 };
 </script>
 
--- frontend/src/views/Mypage/MyWords.vue
+++ frontend/src/views/Mypage/MyWords.vue
@@ -2,190 +2,118 @@
   <!-- <h3 class="title ml-3 px-5">스크랩한 단어</h3> -->
   <!-- 언어 필터링 -->
   <div>
-  <v-container>
-    <v-container style="text-align:center;">
-      <v-row no-gutters>
-        <!-- 정렬 맞추기 위해 왼쪽 빈칸 사용 -->
-        <!-- <v-col cols="12" sm="1"></v-col> -->
-        <!-- @click="onLanguage(category.name, category.key)" -->
-        <!-- <v-col v-for="(lang, i) in langLst" :key="i" cols="12" sm="1">
-            <v-card
-              class="pa-2 categoryCard"
-              tile
-            >
-              <p class="mb-0 categoryTag">{{lang}}</p>
-            </v-card>
-          </v-col> -->
-        <v-col class="mx-2" v-for="(lang, i) in langLst" :key="i" cols=2>
-          <v-btn color="white" miduam width=120 class="truncate" outlined text-color="white" @click="wordList(lang, i)" id="btnlang" :style="{backgroundColor : index[i] ? 'red' : 'black'}">
-            {{ lang.substring(0, 8)  }}
-          </v-btn>
-        </v-col>
-      </v-row>
-      <hr style="width: 100%" class="mt-4">
-    </v-container>
-
-    <v-row v-if="wordlang" class="mx-3" justify="center">
-      <v-container>
-        <!-- 언어별 단어장 -->
-        <!-- expander 사용예정 / expand => 뜻/예문 보이기 -->
-        <!-- <h2 class="mb-3">영어</h2>
-        <v-row no-gutters style="text-align:center; width:70%;">
-          <v-col v-for="i in 6" :key="i" cols="12" sm="6">
-            <v-card class="pa-2 selectLang" outlined tile>
-              <p class="my-auto">{{ i }}</p>
-            </v-card>
-          </v-col>
-        </v-row> -->
-        <h2 class="mt-5 mb-3">{{selectlang}}</h2>
-        <div style="display: table; width: 100%;">
-          <div style="display: table-cell; width: 40%">
-            <!-- <h4>학습중</h4> -->
-            <!-- <v-row no-gutters style="text-align:center; width:70%;">
-              <v-col v-for="(word, i) in wlist" :key="i" cols="12" md="10"> -->
-            <!-- <div v-if="!word.isLearn"> -->
-            <v-expansion-panels accordion  style=" width:70%;">
-              <v-expansion-panel
-                v-for="(word, i) in wlist"
-                :key="i"
-                cols="12"
-                md="10"
+    <v-container>
+      <v-container style="text-align: center">
+        <v-row no-gutters>
+          <swiper style="width: 95%" :options="swiperOption">
+            <swiper-slide v-for="(lang, i) in langLst" :key="i" cols="2">
+              <v-btn
+                color="white"
+                miduam
+                width="120"
+                class="truncate"
+                outlined
+                text-color="white"
+                @click="wordList(lang, i)"
+                id="btnlang"
+                :style="{ backgroundColor: index[i] ? 'red' : 'black' }"
               >
-                <!-- <draggable class="list-group" :list="wlist" group="people" @change="logtoCompleted(word)"> -->
+                {{ lang.substring(0, 8) }}
+              </v-btn>
+            </swiper-slide>
+          </swiper>
+        </v-row>
+        <hr style="width: 100%" class="mt-4" />
+      </v-container>
+
+      <v-row v-if="wordlang" class="mx-3" justify="center">
+        <v-container style="width: 50%; display: inline-block">
+          <h2 class="mb-3">학습중</h2>
+          <div style="display: table; width: 100%">
+            <div style="display: table-cell; width: 40%">
+              <v-expansion-panels
+                :accordion="panel"
+                style="width: 100%"
+                v-model="panel"
+              >
+                <v-expansion-panel
+                  v-for="(word, i) in wlist"
+                  :key="i"
+                  cols="12"
+                  md="10"
+                >
                   <v-expansion-panel-header>
                     {{ word.eachVoca }}
                   </v-expansion-panel-header>
                   <v-expansion-panel-content>
                     {{ word.eachMean }}
+                    <v-icon
+                      medium
+                      style="float: right; color: red"
+                      @click="delWord(word.vocaId, word.country)"
+                      >mdi-close</v-icon
+                    >
+                    <v-icon
+                      medium
+                      style="float: right; color: green"
+                      @click="Check(word.vocaId, word.country)"
+                      >mdi-check</v-icon
+                    >
                   </v-expansion-panel-content>
-                <!-- </draggable> -->
-              </v-expansion-panel>
-            </v-expansion-panels>
-
-            <!-- <transition name="mode-fade" mode="out-in"> -->
-            <!-- key="show" -->
-            <!-- <v-card
-                    v-if="show"
-                    @click="show = false"
-                    class="pa-2 selectLang"
-                    outlined
-                    tile
-                    style="position:relative"
-                  > -->
-            <!-- append-icon="mdi-lead-pencil" label="복습" -->
-            <!-- <v-checkbox
-                      style="width: 27%;
-                        position: absolute;
-                        bottom: 7px;
-                left: 5px;"
-                      color="success"
-                      value="success"
-                      hide-details
-                    ></v-checkbox> -->
-
-            <!-- <v-icon small left>mdi-lead-pencil</v-icon>
-              복습 -->
-
-            <!-- <p class="my-auto">{{ word.eachVoca }}</p>
-                  </v-card> -->
-            <!-- </transition> -->
-            <!-- <transition name="fade" mode="out-in"> -->
-            <!-- key="show" -->
-            <!-- <v-card
-                    v-else
-                    @click="show = true"
-                    class="pa-2 selectLang"
-                    outlined
-                    tile
-                    style="position:relative"
-                  >
-                    append-icon="mdi-lead-pencil" label="복습" -->
-            <!-- <v-checkbox
-                      style="width: 27%;
-                        position: absolute;
-                        bottom: 7px;
-                left: 5px;"
-                      color="success"
-                      value="success"
-                      hide-details
-                    ></v-checkbox> -->
-
-            <!-- <v-icon small left>mdi-lead-pencil</v-icon>
-              복습 -->
-
-            <!-- <p class="my-auto">{{ word.eachMean }}</p>
-                  </v-card>
-                </transition> -->
-            <!-- </div> -->
-            <!-- </v-col>
-            </v-row> -->
+                </v-expansion-panel>
+              </v-expansion-panels>
+            </div>
           </div>
-
-          <!-- <div style="display: table-cell; width: 40%;">
-            <h4>학습완료</h4>
-            <v-expansion-panels accordion style=" width:70%;">
+        </v-container>
+        <v-container style="width: 50%; display: inline-block">
+          <h2 class="mb-3">학습완료</h2>
+          <div style="display: table; width: 100%">
+            <div style="display: table-cell; width: 40%">
+              <v-expansion-panels :accordion="panel" style="width: 100%">
                 <v-expansion-panel
                   v-for="(word, i) in wlist2"
                   :key="i"
                   cols="12"
                   md="10"
                 >
-              <draggable class="list-group" :list="wlist2" group="people">
                   <v-expansion-panel-header>
                     {{ word.eachVoca }}
                   </v-expansion-panel-header>
                   <v-expansion-panel-content>
                     {{ word.eachMean }}
+                    <v-icon
+                      medium
+                      style="float: right; color: red"
+                      @click="delWord(word.vocaId, word.country)"
+                      >mdi-close</v-icon
+                    >
+                    <v-icon
+                      medium
+                      style="float: right; color: gray"
+                      @click="Check(word.vocaId, word.country)"
+                    >
+                      mdi-replay
+                    </v-icon>
                   </v-expansion-panel-content>
-              </draggable>
                 </v-expansion-panel>
-            </v-expansion-panels> -->
-
-            <!-- <v-row no-gutters style="text-align:center; width:70%;"> -->
-              <!-- <p>word</p> -->
-              <!-- <v-col v-for="(word, i) in wlist2" :key="i" cols="12" md="10"> -->
-                <!-- v-if="word.isLearn" -->
-                <!-- <v-card
-                  class="pa-2 selectLang"
-                  outlined
-                  tile
-                  style="position:relative"
-                > -->
-                  <!-- append-icon="mdi-lead-pencil" label="복습" -->
-                  <!-- <v-checkbox
-                    style="width: 27%;
-                position: absolute;
-                bottom: 7px;
-                left: 5px;"
-                    color="success"
-                    value="success"
-                    hide-details
-                  ></v-checkbox> -->
-
-                  <!-- <v-icon small left>mdi-lead-pencil</v-icon>
-              복습 -->
-
-                  <!-- <p class="my-auto">{{ word.eachVoca }}</p>
-                </v-card>
-              </v-col> -->
-            <!-- </v-row> -->
-          <!-- </div> -->
-        </div>
-      </v-container>
-    </v-row>
-  </v-container>
-  <v-row v-if="wordlang" class="px-5 mt-5 mx-3" justify="center">
-
-<!-- 
+              </v-expansion-panels>
+            </div>
+          </div>
+        </v-container>
+      </v-row>
+    </v-container>
+    <v-row v-if="wordlang" class="px-5 mt-5 mx-3" justify="center">
+      <!-- 
     <rawDisplayer class="col-3" :value="list1" title="List 1" />
 
     <rawDisplayer class="col-3" :value="list2" title="List 2" /> -->
-  </v-row>
+    </v-row>
   </div>
-
 </template>
 <script>
 import axios from "@/plugins/axios";
+import "swiper/swiper-bundle.css";
+import { Swiper, SwiperSlide } from "vue-awesome-swiper";
 // import draggable from "vuedraggable";
 
 export default {
@@ -194,74 +122,94 @@ export default {
   order: 1,
   components: {
     // draggable
+    Swiper,
+    SwiperSlide,
   },
   data() {
     return {
+      panel: false,
+      swiperOption: {
+        slidesPerView: 4,
+        spaceBetween: 0,
+      },
       index: [],
       show: true,
       wordlist: [],
       selectlang: "",
       wordlang: false,
       langLst: [],
-      // language: {
-      //   en: "영어",
-      //   jp: "일본어",
-      //   cn: "중국어",
-      //   vi: "베트남어",
-      //   id: "인도네시아어",
-      //   ar: "아랍어",
-      //   bn: "뱅갈어",
-      //   de: "독일어",
-      //   es: "스페인어",
-      //   fr: "프랑스어",
-      //   hi: "힌디어",
-      //   it: "이탈리아어",
-      //   ms: "말레이시아어",
-      //   nl: "네덜란드어",
-      //   pt: "포르투갈어",
-      //   ru: "러시아",
-      //   th: "태국어",
-      //   tr: "터키어"
-      // }
+      wlist: [],
+      wlist2: [],
     };
   },
   computed: {
-    wlist: function() {
-      return this.wordlist.filter(function(w) {
-        return !w.isLearn;
-      });
-    },
-    wlist2: function() {
-      return this.wordlist.filter(function(w) {
-        return w.isLearn;
-      });
-    }
+    // wlist: function () {
+    //   return this.wordlist.filter(function (w) {
+    //     return !w.isLearn;
+    //   });
+    // },
+    // wlist2: function () {
+    //   return this.wordlist.filter(function (w) {
+    //     return w.isLearn;
+    //   });
+    // },
   },
   methods: {
-    clone: function(el) {
-      console.log('clone함수');
+    async Check(id, country) {
+      await axios.put(`/user/islearn?vocaId=${id}`);
+      this.Next(country);
+    },
+    async Next(country) {
+      this.panel = false;
+      await axios
+        .get(`/user/myvoca?country=${country}&direction=ASC&page=0&size=1000`)
+        .then((res) => {
+          console.log(res);
+          this.wordlist = res.data.content;
+          this.wlist = [];
+          this.wlist2 = [];
+          for (var i = 0; i < this.wordlist.length; i++) {
+            if (this.wordlist[i].isLearn) {
+              this.wlist2.push(this.wordlist[i]);
+              console.log("돌아라");
+              console.log(i);
+            } else {
+              this.wlist.push(this.wordlist[i]);
+            }
+          }
+          // console.log("언어별 단어장");
+        });
+    },
+    async delWord(wid, country) {
+      // var flag = 0;
+      await axios.delete(`/user/delete-voca?vocaId=${wid}`);
+      this.Next(country);
+    },
+    clone: function (el) {
+      console.log("clone함수");
       return {
-        name: el.name + " cloned"
+        name: el.name + " cloned",
       };
     },
-    logtoProgress: function(word) {
-      console.log('logto Progess함수');
+    logtoProgress: function (word) {
+      console.log("logto Progess함수");
       word.isLearn = false;
       console.log(word);
     },
-    logtoCompleted: function(word) {
-      console.log('logtoCompleted함수');
+    logtoCompleted: function (word) {
+      console.log("logtoCompleted함수");
       word.isLearn = true;
       console.log(word);
     },
     wordList(lang, idx) {
+      this.wlist = [];
+      this.wlist2 = [];
       // var btn = document.getElementById("btnlang")
-      for(var i=0; i < this.index.length; i ++ ) {
-        if(i == idx) {
-          this.index[i] = !this.index[i]
-        }
-        else {
-          this.index[i] = false
+      for (var i = 0; i < this.index.length; i++) {
+        if (i == idx) {
+          this.index[i] = !this.index[i];
+        } else {
+          this.index[i] = false;
         }
       }
       if (lang == this.selectlang) {
@@ -273,31 +221,39 @@ export default {
       this.selectlang = lang;
       axios
         .get(`/user/myvoca?country=${lang}&direction=ASC&page=0&size=10`)
-        .then(res => {
+        .then((res) => {
           console.log(res);
           this.wordlist = res.data.content;
           console.log("언어별 단어장");
+          for (var i = 0; i < this.wordlist.length; i++) {
+            if (this.wordlist[i].isLearn) {
+              this.wlist2.push(this.wordlist[i]);
+            } else {
+              this.wlist.push(this.wordlist[i]);
+            }
+          }
         });
-    }
+      this.panel = false;
+    },
   },
   mounted() {
-      axios.get("/user/myvoca-country").then(res => {
-        console.log("단어장에 저장된 국가들");
-        console.log(res);
-        this.langLst = res.data;
-        console.log("길이")
-        console.log(res.data.length)
-        for (var i=0; i< res.data.length; i++ ) {
-          this.index.push(false)
-        }
-        console.log(this.index)
-      });
-    },
+    axios.get("/user/myvoca-country").then((res) => {
+      console.log("단어장에 저장된 국가들");
+      console.log(res);
+      this.langLst = res.data;
+      console.log("길이");
+      console.log(res.data.length);
+      for (var i = 0; i < res.data.length; i++) {
+        this.index.push(false);
+      }
+      console.log(this.index);
+    });
+  },
 };
 </script>
 
 <style scoped>
-  .title {
+.title {
   color: black;
 }
 
@@ -328,6 +284,4 @@ export default {
   text-overflow: ellipsis;
   white-space: nowrap;
 }
-
-
 </style>
